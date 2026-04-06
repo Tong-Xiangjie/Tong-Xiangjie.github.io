920fc0148d002036da16983e3b08a2e843706c91
Tong-Xiangjie <2011746785@qq.com>
2026-04-06 13:05:08 +0800

    Enhance hk_scData with detailFields and series
    Added detailFields for banknote information and expanded series data.

--- notecollection/data/hk_sc.js
+++ notecollection/data/hk_sc.js
@@ -3,5 +3,237 @@ const hk_scData = {
     name: "香港渣打银行",
     icon: "𝟘𝟜",
     desc: "香港渣打银行纸币",
-    series: []
+    // 港币板块的详情页字段配置
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "year", label: "发行年份" },
+        { key: "signature", label: "签名" },
+        { key: "faceDate", label: "票面日期" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    series: [
+        {
+            seriesName: "1979～1982年版 港币10元",
+            year: "1979～1982",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1981,
+                    version: "CW609645",
+                    bank: "香港渣打银行",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "77b",
+                    signature: "",
+                    faceDate: "1981年1月1日",
+                    remark: "",
+                    img1: "hk_sc/CW609645-1.jpg",
+                    img2: "hk_sc/CW609645-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1993～2002年版 港币10元",
+            year: "1993～2002",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1995,
+                    version: "CU891135",
+                    bank: "香港渣打银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "284b",
+                    signature: "",
+                    faceDate: "1995年1月1日",
+                    remark: "",
+                    img1: "hk_sc/CU891135-1.jpg",
+                    img2: "hk_sc/CU891135-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1993～2002年版 港币20元",
+            year: "1993～2002",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1995,
+                    version: "AZ118002",
+                    bank: "香港渣打银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "285b",
+                    signature: "香港分行总经理：周振兴",
+                    faceDate: "1995年1月1日",
+                    remark: "",
+                    img1: "hk_sc/AZ118002-1.jpg",
+                    img2: "hk_sc/AZ118002-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1993～2002年版 港币50元",
+            year: "1993～2002",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1994,
+                    version: "B070293",
+                    bank: "香港渣打银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "286b",
+                    signature: "",
+                    faceDate: "1994年1月1日",
+                    remark: "",
+                    img1: "hk_sc/B070293-1.jpg",
+                    img2: "hk_sc/B070293-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 1995,
+                    version: "G844653",
+                    bank: "香港渣打银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "286b",
+                    signature: "",
+                    faceDate: "1995年1月1日",
+                    remark: "",
+                    img1: "hk_sc/G844653-1.jpg",
+                    img2: "hk_sc/G844653-2.jpg"
+                },
+                {
+                    copyId: 3,
+                    year: 2001,
+                    version: "AP720645",
+                    bank: "香港渣打银行",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "286c",
+                    signature: "",
+                    faceDate: "2001年1月1日",
+                    remark: "",
+                    img1: "hk_sc/AP720645-1.jpg",
+                    img2: "hk_sc/AP720645-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2010～2016年版 港币20元",
+            year: "2010～2016",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2016,
+                    version: "FK389601",
+                    bank: "渣打银行（香港）",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "297e",
+                    signature: "",
+                    faceDate: "2016年1月1日",
+                    remark: "",
+                    img1: "hk_sc/FK389601-1.jpg",
+                    img2: "hk_sc/FK389601-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2010～2016年版 港币100元",
+            year: "2010～2016",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2010,
+                    version: "AB131676",
+                    bank: "渣打银行（香港）",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "299a",
+                    signature: "",
+                    faceDate: "2010年1月1日",
+                    remark: "",
+                    img1: "hk_sc/AB131676-1.jpg",
+                    img2: "hk_sc/AB131676-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018～至今版 港币20元",
+            year: "2018～至今",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2023,
+                    version: "DL561100",
+                    bank: "渣打银行（香港）",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "302c",
+                    signature: "",
+                    faceDate: "2023年1月1日",
+                    remark: "",
+                    img1: "hk_sc/DL561100-1.jpg",
+                    img2: "hk_sc/DL561100-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018～至今版 港币50元",
+            year: "2018～至今",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2023,
+                    version: "BS603532",
+                    bank: "渣打银行（香港）",
+                    condition: "ACG68E★",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "303c",
+                    signature: "",
+                    faceDate: "2023年1月1日",
+                    remark: "",
+                    img1: "hk_sc/BS603532-1.jpg",
+                    img2: "hk_sc/BS603532-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018～至今版 港币100元",
+            year: "2018～至今",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2020,
+                    version: "BE308101",
+                    bank: "渣打银行（香港）",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "304b",
+                    signature: "",
+                    faceDate: "2020年1月1日",
+                    remark: "",
+                    img1: "hk_sc/BE308101-1.jpg",
+                    img2: "hk_sc/BE308101-2.jpg"
+                }
+            ]
+        }
+    ]
 };
