f70309d826d9ec9a011f4abda6139d11abda8541
Tong-Xiangjie <2011746785@qq.com>
2026-04-06 18:30:03 +0800

    Update rmb5.js

--- notecollection/data/rmb5.js
+++ notecollection/data/rmb5.js
@@ -3,5 +3,420 @@ const rmb5Data = {
     name: "第五套人民币",
     icon: "𝟙𝟛",
     desc: "第五套人民币",
-    series: []
+    // 五版币板块的详情页字段配置
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "year", label: "发行年份" },
+        { key: "issueDate", label: "发行日期" },
+        { key: "wmk", label: "水印" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    series: [
+        {
+            seriesName: "1999年版 1元",
+            year: "1999",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1999,
+                    version: "OH11117778",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "895a",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/OH11117778-1.jpg",
+                    img2: "image/rmb5/OH11117778-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 1999,
+                    version: "DN02216986",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "895a",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "大叶兰",
+                    img1: "image/rmb5/DN02216986-1.jpg",
+                    img2: "image/rmb5/DN02216986-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1999年版 5元",
+            year: "1999",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1999,
+                    version: "PF93563125",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "897",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/PF93563125-1.jpg",
+                    img2: "image/rmb5/PF93563125-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1999年版 10元",
+            year: "1999",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1999,
+                    version: "TB96818893",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "898",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "小圆水",
+                    img1: "image/rmb5/TB96818893-1.jpg",
+                    img2: "image/rmb5/TB96818893-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 1999,
+                    version: "YG89660155",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "898",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "宽水",
+                    img1: "image/rmb5/YG89660155-1.jpg",
+                    img2: "image/rmb5/YG89660155-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1999年版 20元",
+            year: "1999",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1999,
+                    version: "JF22966632",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "899",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/JF22966632-1.jpg",
+                    img2: "image/rmb5/JF22966632-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1999年版 50元",
+            year: "1999",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1999,
+                    version: "BB31832995",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "900",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/BB31832995-1.jpg",
+                    img2: "image/rmb5/BB31832995-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1999年版 100元",
+            year: "1999",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1999,
+                    version: "QQ16191321",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "901",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/QQ16191321-1.jpg",
+                    img2: "image/rmb5/QQ16191321-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2005年版 5元",
+            year: "2005",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2005,
+                    version: "IO88888767",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "903a",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/IO88888767-1.jpg",
+                    img2: "image/rmb5/IO88888767-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2005年版 10元",
+            year: "2005",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2005,
+                    version: "UL00089069",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "904a",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/UL00089069-1.jpg",
+                    img2: "image/rmb5/UL00089069-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2005年版 20元",
+            year: "2005",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2005,
+                    version: "PY44444484",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "905",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/PY44444484-1.jpg",
+                    img2: "image/rmb5/PY44444484-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2005年版 50元",
+            year: "2005",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2005,
+                    version: "XY08981095",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "906*",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/XY08981095-1.jpg",
+                    img2: "image/rmb5/XY08981095-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2005年版 100元",
+            year: "2005",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2005,
+                    version: "OX12345659",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "907a",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/OX12345659-1.jpg",
+                    img2: "image/rmb5/OX12345659-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2019年版 1元",
+            year: "2019",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2019,
+                    version: "FA42054438",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "912a",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/FA42054438-1.jpg",
+                    img2: "image/rmb5/FA42054438-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 2019,
+                    version: "(33333454)",
+                    bank: "中国人民银行",
+                    condition: "ACG67+E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "912a",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/33333454-1.jpg",
+                    img2: "image/rmb5/33333454-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2020年版 5元",
+            year: "2020",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2020,
+                    version: "AH58603088",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "913*",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/AH58603088-1.jpg",
+                    img2: "image/rmb5/AH58603088-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2019年版 10元",
+            year: "2019",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2019,
+                    version: "TF19999669",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "914a",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/TF19999669-1.jpg",
+                    img2: "image/rmb5/TF19999669-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2019年版 20元",
+            year: "2019",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2019,
+                    version: "JE19999669",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "915a",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/JE19999669-1.jpg",
+                    img2: "image/rmb5/JE19999669-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2019年版 50元",
+            year: "2019",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2019,
+                    version: "0",
+                    bank: "中国人民银行",
+                    condition: "ACG00E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "916a",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "占位，暂无藏品",
+                    img1: "",
+                    img2: ""
+                }
+            ]
+        },
+        {
+            seriesName: "2015年版 100元",
+            year: "2015",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2015,
+                    version: "B000S00055",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "909a.4",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb5/B000S00055-1.jpg",
+                    img2: "image/rmb5/B000S00055-2.jpg"
+                }
+            ]
+        }
+    ]
 };
