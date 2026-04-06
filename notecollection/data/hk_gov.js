ea50e1cc96092b455e89fe9d981921954ae30202
Tong-Xiangjie <2011746785@qq.com>
2026-04-06 12:23:45 +0800

    Enhance hk_gov.js with detailFields and series data
    Added detailFields for currency configuration and expanded series data for Hong Kong government banknotes.

--- notecollection/data/hk_gov.js
+++ notecollection/data/hk_gov.js
@@ -3,5 +3,207 @@ const hk_govData = {
     name: "香港政府",
     icon: "𝟘𝟝",
     desc: "香港政府纸币",
-    series: []
+    // 港币板块的详情页字段配置
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "year", label: "发行年份" },
+        { key: "signature", label: "签名" },
+        { key: "faceDate", label: "票面日期" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    series: [
+        {
+            seriesName: "2002年版 港币10元",
+            year: "2002",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2002,
+                    version: "DD954229",
+                    bank: "香港政府",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "400a",
+                    signature: "财政司司长：梁锦松<br>金融管理专员：任志刚",
+                    faceDate: "2002年7月1日",
+                    remark: "",
+                    img1: "hk_gov/DD954229-1.jpg",
+                    img2: "hk_gov/DD954229-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2003年版 港币10元",
+            year: "2003",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2003,
+                    version: "0",
+                    bank: "香港政府",
+                    condition: "ACG00E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "400b",
+                    signature: "财政司司长：梁锦松<br>金融管理专员：任志刚",
+                    faceDate: "2003年1月1日",
+                    remark: "占位，暂无藏品",
+                    img1: "",
+                    img2: ""
+                }
+            ]
+        },
+        {
+            seriesName: "2005年版 港币10元",
+            year: "2005",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2005,
+                    version: "SK706047",
+                    bank: "香港政府",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "400c",
+                    signature: "财政司司长：唐英年<br>金融管理专员：任志刚",
+                    faceDate: "2005年1月1日",
+                    remark: "",
+                    img1: "hk_gov/SK706047-1.jpg",
+                    img2: "hk_gov/SK706047-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2007年4月版 港币10元",
+            year: "2007",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2007,
+                    version: "0",
+                    bank: "香港政府",
+                    condition: "ACG00E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "401a",
+                    signature: "财政司司长：唐英年<br>金融管理专员：任志刚",
+                    faceDate: "2007年4月1日",
+                    remark: "占位，暂无藏品",
+                    img1: "",
+                    img2: ""
+                }
+            ]
+        },
+        {
+            seriesName: "2007年10月版 港币10元",
+            year: "2007",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2007,
+                    version: "PG293444",
+                    bank: "香港政府",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "401b",
+                    signature: "财政司司长：曾俊华<br>金融管理专员：任志刚",
+                    faceDate: "2007年10月1日",
+                    remark: "",
+                    img1: "hk_gov/PG293444-1.jpg",
+                    img2: "hk_gov/PG293444-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2012年版 港币10元",
+            year: "2012",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2012,
+                    version: "UJ104777",
+                    bank: "香港政府",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "401c",
+                    signature: "财政司司长：曾俊华<br>金融管理专员：陈德霖",
+                    faceDate: "2012年1月1日",
+                    remark: "",
+                    img1: "hk_gov/UJ104777-1.jpg",
+                    img2: "hk_gov/UJ104777-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2014年版 港币10元",
+            year: "2014",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2014,
+                    version: "YR735333",
+                    bank: "香港政府",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "401d",
+                    signature: "财政司司长：曾俊华<br>金融管理专员：陈德霖",
+                    faceDate: "2014年1月1日",
+                    remark: "",
+                    img1: "hk_gov/YR735333-1.jpg",
+                    img2: "hk_gov/YR735333-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018年版 港币10元",
+            year: "2018",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2018,
+                    version: "DH136631",
+                    bank: "香港政府",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "401e",
+                    signature: "财政司司长：陈茂波<br>金融管理专员：陈德霖",
+                    faceDate: "2018年7月1日",
+                    remark: "",
+                    img1: "hk_gov/DH136631-1.jpg",
+                    img2: "hk_gov/DH136631-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2024年版 港币10元",
+            year: "2024",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2024,
+                    version: "HL395991",
+                    bank: "香港政府",
+                    condition: "ACG69E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "401f",
+                    signature: "财政司司长：陈茂波<br>金融管理专员：余伟文",
+                    faceDate: "2024年1月1日",
+                    remark: "",
+                    img1: "hk_gov/HL395991-1.jpg",
+                    img2: "hk_gov/HL395991-2.jpg"
+                }
+            ]
+        }
+    ]
 };
