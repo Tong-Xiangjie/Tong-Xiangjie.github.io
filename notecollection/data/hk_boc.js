1cfa6f3106a35c8cf84cb2abc1d69f0cad87de2e
Tong-Xiangjie <2011746785@qq.com>
2026-04-06 10:28:58 +0800

    Add files via upload

--- notecollection/data/hk_boc.js
+++ notecollection/data/hk_boc.js
@@ -2,6 +2,259 @@
 const hk_bocData = {
     name: "香港中国银行",
     icon: "𝟘𝟚",
-    desc: "香港中国银行纸币",
-    series: []
+    desc: "Bank of China (Hong Kong Limited)",
+    // 港币板块的详情页字段配置
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "year", label: "发行年份" },
+        { key: "signature", label: "签名" },
+        { key: "faceDate", label: "票面日期" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    series: [
+        {
+            seriesName: "1994～2001年版 港币20元",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1994,
+                    version: "AA775221",
+                    bank: "中国银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "329a",
+                    signature: "香港分行总经理：周振兴",
+                    faceDate: "1994年5月1日",
+                    remark: "首发冠",
+                    img1: "hk_boc/AA775221-1.jpg",
+                    img2: "hk_boc/AA775221-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1994～2001年版 港币50元",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1996,
+                    version: "AF724347",
+                    bank: "中国银行",
+                    condition: "ACG66E",
+                    price: "88元",
+                    purchaseDate: "2026年2月19日",
+                    krause: "330b",
+                    signature: "香港分行总经理：羊子林",
+                    faceDate: "1996年1月1日",
+                    remark: "",
+                    img1: "hk_boc/AF724347-1.jpg",
+                    img2: "hk_boc/AF724347-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 1999,
+                    version: "AZ261561",
+                    bank: "中国银行",
+                    condition: "ACG67E",
+                    price: "88元",
+                    purchaseDate: "2026年2月19日",
+                    krause: "330e",
+                    signature: "香港分行总经理：刘金宝",
+                    faceDate: "1999年1月1日",
+                    remark: "",
+                    img1: "hk_boc/AZ261561-1.jpg",
+                    img2: "hk_boc/AZ261561-2.jpg"
+                },
+                {
+                    copyId: 3,
+                    year: 2000,
+                    version: "AT691569",
+                    bank: "中国银行",
+                    condition: "ACG66E",
+                    price: "88元",
+                    purchaseDate: "2026年2月19日",
+                    krause: "330f",
+                    signature: "香港分行总经理：刘金宝",
+                    faceDate: "2000年1月1日",
+                    remark: "",
+                    img1: "hk_boc/AT691569-1.jpg",
+                    img2: "hk_boc/AT691569-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1994～2001年版 港币100元",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2000,
+                    version: "CA253979",
+                    bank: "中国银行",
+                    condition: "ACG67E",
+                    price: "145元",
+                    purchaseDate: "2026年2月19日",
+                    krause: "331f",
+                    signature: "香港分行总经理：刘金宝",
+                    faceDate: "2000年1月1日",
+                    remark: "",
+                    img1: "hk_boc/CA253979-1.jpg",
+                    img2: "hk_boc/CA253979-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2003～2009年版 港币20元",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2008,
+                    version: "HB778596",
+                    bank: "中国银行（香港）",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "335e",
+                    signature: "总裁：和广北",
+                    faceDate: "2008年1月1日",
+                    remark: "",
+                    img1: "hk_boc/HB778596-1.jpg",
+                    img2: "hk_boc/HB778596-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2003～2009年版 港币50元",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2009,
+                    version: "DB198972",
+                    bank: "中国银行（香港）",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "336f",
+                    signature: "总裁：和广北",
+                    faceDate: "2009年1月1日",
+                    remark: "",
+                    img1: "hk_boc/DB198972-1.jpg",
+                    img2: "hk_boc/DB198972-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2010～2015年版 港币20元",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2015,
+                    version: "JF775298",
+                    bank: "中国银行（香港）",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "341e",
+                    signature: "总裁：岳毅",
+                    faceDate: "2015年7月1日",
+                    remark: "",
+                    img1: "hk_boc/JF775298-1.jpg",
+                    img2: "hk_boc/JF775298-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018～至今版 港币20元",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2018,
+                    version: "AW102222",
+                    bank: "中国银行（香港）",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "348a",
+                    signature: "总裁：高迎欣",
+                    faceDate: "2018年1月1日",
+                    remark: "",
+                    img1: "hk_boc/AW102222-1.jpg",
+                    img2: "hk_boc/AW102222-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 2023,
+                    version: "JA601300",
+                    bank: "中国银行（香港）",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "348c",
+                    signature: "总裁：孙煜",
+                    faceDate: "2023年1月1日",
+                    remark: "",
+                    img1: "hk_boc/JA601300-1.jpg",
+                    img2: "hk_boc/JA601300-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018～至今版 港币50元",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2023,
+                    version: "BR046520",
+                    bank: "中国银行（香港）",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "349",
+                    signature: "总裁：孙煜",
+                    faceDate: "2023年1月1日",
+                    remark: "",
+                    img1: "hk_boc/BR046520-1.jpg",
+                    img2: "hk_boc/BR046520-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018～至今版 港币100元",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2018,
+                    version: "AA739695",
+                    bank: "中国银行（香港）",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "350a",
+                    signature: "总裁：高迎欣",
+                    faceDate: "2018年1月1日",
+                    remark: "",
+                    img1: "hk_boc/AA739695-1.jpg",
+                    img2: "hk_boc/AA739695-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 2023,
+                    version: "EX015191",
+                    bank: "中国银行（香港）",
+                    condition: "ACG68E★",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "350",
+                    signature: "总裁：孙煜",
+                    faceDate: "2023年1月1日",
+                    remark: "",
+                    img1: "hk_boc/EX015191-1.jpg",
+                    img2: "hk_boc/EX015191-2.jpg"
+                }
+            ]
+        }
+    ]
 };
