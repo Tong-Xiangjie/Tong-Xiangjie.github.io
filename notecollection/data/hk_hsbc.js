13424fa14df0b7f341bb5d8e0d24557e78fc5ba0
Tong-Xiangjie <2011746785@qq.com>
2026-04-06 17:50:58 +0800

    Update hk_hsbc.js

--- notecollection/data/hk_hsbc.js
+++ notecollection/data/hk_hsbc.js
@@ -2,6 +2,253 @@
 const hk_hsbcData = {
     name: "香港汇丰银行",
     icon: "𝟘𝟛",
-    desc: "香港汇丰银行纸币",
-    series: []
+    desc: "The Hong Kong and Shanghai Banking Corporation",
+    // 港币板块的详情页字段配置
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "year", label: "发行年份" },
+        { key: "signature", label: "签名" },
+        { key: "faceDate", label: "票面日期" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    series: [
+        {
+            seriesName: "1985～1992年版 港币10元",
+            year: "1985～1992",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1992,
+                    version: "VG198217",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "191c.4",
+                    signature: "",
+                    faceDate: "1992年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/VG198217-1.jpg",
+                    img2: "image/hk_hsbc/VG198217-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1985～1992年版 港币20元",
+            year: "1985～1992",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1989,
+                    version: "BL611954",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "192c",
+                    signature: "",
+                    faceDate: "1989年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/BL611954-1.jpg",
+                    img2: "image/hk_hsbc/BL611954-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2010～2016年版 港币20元 中秋",
+            year: "2010～2016",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2013,
+                    version: "LB666960",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "212c",
+                    signature: "",
+                    faceDate: "2013年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/LB666960-1.jpg",
+                    img2: "image/hk_hsbc/LB666960-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 2014,
+                    version: "PF188690",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "212d",
+                    signature: "",
+                    faceDate: "2014年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/PF188690-1.jpg",
+                    img2: "image/hk_hsbc/PF188690-2.jpg"
+                },
+                {
+                    copyId: 3,
+                    year: 2016,
+                    version: "YT555510",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "212e",
+                    signature: "",
+                    faceDate: "2016年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/YT555510-1.jpg",
+                    img2: "image/hk_hsbc/YT555510-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2010～2016年版 港币50元 元宵",
+            year: "2010～2016",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2010,
+                    version: "BB926286",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "213a",
+                    signature: "",
+                    faceDate: "2010年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/BB926286-1.jpg",
+                    img2: "image/hk_hsbc/BB926286-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2010～2016年版 港币100元 回归",
+            year: "2010～2016",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2010,
+                    version: "DC908607",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "214a",
+                    signature: "",
+                    faceDate: "2010年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/DC908607-1.jpg",
+                    img2: "image/hk_hsbc/DC908607-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018～至今版 港币20元 一家茶叙",
+            year: "2018～至今",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2018,
+                    version: "GB308600",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "218a",
+                    signature: "",
+                    faceDate: "2018年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/GB308600-1.jpg",
+                    img2: "image/hk_hsbc/GB308600-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018～至今版 港币50元 蝶舞花间",
+            year: "2018～至今",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2020,
+                    version: "CZ388408",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "219b",
+                    signature: "",
+                    faceDate: "2020年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/CZ388408-1.jpg",
+                    img2: "image/hk_hsbc/CZ388408-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018～至今版 港币100元 梨园粤韵",
+            year: "2018～至今",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2018,
+                    version: "AE102558",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "220a",
+                    signature: "",
+                    faceDate: "2018年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/AE102558-1.jpg",
+                    img2: "image/hk_hsbc/AE102558-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 2022,
+                    version: "HE180186",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "220c",
+                    signature: "",
+                    faceDate: "2022年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/HE180186-1.jpg",
+                    img2: "image/hk_hsbc/HE180186-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018～至今版 港币500元 六角岩柱",
+            year: "2018～至今",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2023,
+                    version: "LB937620",
+                    bank: "香港上海汇丰银行",
+                    condition: "ACG68E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "221",
+                    signature: "",
+                    faceDate: "2023年1月1日",
+                    remark: "",
+                    img1: "image/hk_hsbc/LB937620-1.jpg",
+                    img2: "image/hk_hsbc/LB937620-2.jpg"
+                }
+            ]
+        }
+    ]
 };
