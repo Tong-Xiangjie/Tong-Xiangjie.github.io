42ea524cb3b7e65c8fa08789a1adb97f73ab4626
Tong-Xiangjie <2011746785@qq.com>
2026-04-07 21:01:43 +0800

    Update gkq.js

--- notecollection/data/gkq.js
+++ notecollection/data/gkq.js
@@ -2,6 +2,36 @@
 const gkqData = {
     name: "国库券",
     icon: "𝟙𝟝",
-    desc: "国库券",
-    series: []
+    desc: "Treasury Bond",
+    // 国库券板块的详情页字段配置
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "year", label: "发行年份" },
+        { key: "wmk", label: "水印" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    series: [
+    {
+            seriesName: "1982年版 1元",
+            year: "1982",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1982,
+                    version: "ⅣⅩ148437",
+                    condition: "ACG65E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "Unlisted",
+                    wmk: "",
+                    remark: "",
+                    img1: "image/gkq/ⅣⅩ148437-1.jpg",
+                    img2: "image/gkq/ⅣⅩ148437-2.jpg"
+                }
+            ]
+        }
+   ]
 };
