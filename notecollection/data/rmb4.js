0a0e700dccbf2811ec938dfd895176f3211a532c
Tong-Xiangjie <2011746785@qq.com>
2026-04-07 20:30:30 +0800

    Update rmb4.js

--- notecollection/data/rmb4.js
+++ notecollection/data/rmb4.js
@@ -3,5 +3,330 @@ const rmb4Data = {
     name: "第四套人民币",
     icon: "𝟙𝟘",
     desc: "Fourth Series of the Renminbi",
-    series: []
+    // 四版币板块的详情页字段配置
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "year", label: "发行年份" },
+        { key: "issueDate", label: "发行日期" },
+        { key: "wmk", label: "水印" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    series: [
+        {
+            seriesName: "1980年版 1角",
+            year: "1980",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1980,
+                    version: "SE11609551",
+                    bank: "中国人民银行",
+                    condition: "ACG66E",
+                    price: "25元",
+                    purchaseDate: "2025年10月17日",
+                    krause: "881b",
+                    wmk: "无水印/Without watermark",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/SE11609551-1.jpg",
+                    img2: "image/rmb4/SE11609551-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1980年版 2角",
+            year: "1980",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1980,
+                    version: "AS29332232",
+                    bank: "中国人民银行",
+                    condition: "ACG68E",
+                    price: "49元",
+                    purchaseDate: "2025年10月30日",
+                    krause: "882",
+                    wmk: "无水印/Without watermark",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/AS29332232-1.jpg",
+                    img2: "image/rmb4/AS29332232-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 1980,
+                    version: "TC61307820",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "26元",
+                    purchaseDate: "2025年10月22日",
+                    krause: "882",
+                    wmk: "无水印/Without watermark",
+                    issueDate: "",
+                    remark: "幼线体",
+                    img1: "image/rmb4/TC61307820-1.jpg",
+                    img2: "image/rmb4/TC61307820-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1980年版 5角",
+            year: "1980",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1980,
+                    version: "AM11901126",
+                    bank: "中国人民银行",
+                    condition: "ACG65E",
+                    price: "10元",
+                    purchaseDate: "2025年10月5日",
+                    krause: "883a",
+                    wmk: "无水印/Without watermark",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/AM11901126-1.jpg",
+                    img2: "image/rmb4/AM11901126-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1980年版 1元",
+            year: "1980",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1980,
+                    version: "GZ93445496",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "28元",
+                    purchaseDate: "2026年2月18日",
+                    krause: "884",
+                    wmk: "满版古币水印/Pu Coin(Pants&Coins)",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/GZ93445496-1.jpg",
+                    img2: "image/rmb4/GZ93445496-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 1980,
+                    version: "JT98150594",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "50元",
+                    purchaseDate: "2025年10月22日",
+                    krause: "884",
+                    wmk: "满版古币水印/Pu Coin(Pants&Coins)",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/JT98150594-1.jpg",
+                    img2: "image/rmb4/JT98150594-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1990年版 1元",
+            year: "1990",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1990,
+                    version: "WO00026774",
+                    bank: "中国人民银行",
+                    condition: "884",
+                    price: "30元",
+                    purchaseDate: "2025年11月24日",
+                    krause: "ACG68E",
+                    wmk: "满版古币水印/Pu Coin(Pants&Coins)",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/WO00026774-1.jpg",
+                    img2: "image/rmb4/WO00026774-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1996年版 1元",
+            year: "1996",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1996,
+                    version: "QE99989731",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "29元",
+                    purchaseDate: "2025年11月24日",
+                    krause: "884g",
+                    wmk: "满版五星水印/Stars",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/QE99989731-1.jpg",
+                    img2: "image/rmb4/QE99989731-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1980年版 2元",
+            year: "1980",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1980,
+                    version: "AR82501713",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "48元",
+                    purchaseDate: "2025年10月22日",
+                    krause: "885a",
+                    wmk: "满版古币水印/Pu Coin(Pants&Coins)",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/AR82501713-1.jpg",
+                    img2: "image/rmb4/AR82501713-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1990年版 2元",
+            year: "1990",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1990,
+                    version: "UB35002347",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "24元",
+                    purchaseDate: "2025年10月6日",
+                    krause: "885b",
+                    wmk: "满版古币水印/Pu Coin(Pants&Coins)",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/UB35002347-1.jpg",
+                    img2: "image/rmb4/UB35002347-2.jpg"
+                },
+                {
+                    copyId: 2,
+                    year: 1990,
+                    version: "HR62925341",
+                    bank: "中国人民银行",
+                    condition: "PMG65E",
+                    price: "95元",
+                    purchaseDate: "2025年10月21日",
+                    krause: "885bf",
+                    wmk: "满版古币水印/Pu Coin(Pants&Coins)",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/HR62925341-1.jpg",
+                    img2: "image/rmb4/HR62925341-2.jpg"
+                },
+                {
+                    copyId: 3,
+                    year: 1990,
+                    version: "WG11911933",
+                    bank: "中国人民银行",
+                    condition: "PMG67E",
+                    price: "168元",
+                    purchaseDate: "2025年10月10日",
+                    krause: "885b",
+                    wmk: "满版古币水印/Pu Coin(Pants&Coins)",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/WG11911933-1.jpg",
+                    img2: "image/rmb4/WG11911933-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1980年版 5元",
+            year: "1980",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1980,
+                    version: "EN36556458",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "29元",
+                    purchaseDate: "2025年10月6日",
+                    krause: "886",
+                    wmk: "满版古币水印/Pu Coin(Pants&Coins)",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/EN36556458-1.jpg",
+                    img2: "image/rmb4/EN36556458-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1980年版 10元",
+            year: "1980",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1980,
+                    version: "YF23802314",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "42元",
+                    purchaseDate: "2025年10月6日",
+                    krause: "887",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/YF23802314-1.jpg",
+                    img2: "image/rmb4/YF23802314-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1990年版 50元",
+            year: "1990",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1990,
+                    version: "TG17299637",
+                    bank: "中国人民银行",
+                    condition: "ACG66E",
+                    price: "123元",
+                    purchaseDate: "2025年10月4日",
+                    krause: "888b",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/TG17299637-1.jpg",
+                    img2: "image/rmb4/TG17299637-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "1990年版 100元",
+            year: "1990",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1990,
+                    version: "XF29286837",
+                    bank: "中国人民银行",
+                    condition: "ACG67E",
+                    price: "160元",
+                    purchaseDate: "2026年2月18日",
+                    krause: "889b",
+                    wmk: "",
+                    issueDate: "",
+                    remark: "",
+                    img1: "image/rmb4/XF29286837-1.jpg",
+                    img2: "image/rmb4/XF29286837-2.jpg"
+                }
+            ]
+        }
+    ]
 };
