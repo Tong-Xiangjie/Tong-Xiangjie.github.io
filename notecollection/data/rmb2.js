9d1ba550167538d152471e118bcda1c06ec11eff
Tong-Xiangjie <2011746785@qq.com>
2026-04-11 13:28:25 +0800

    Update rmb2.js

--- notecollection/data/rmb2.js
+++ notecollection/data/rmb2.js
@@ -13,7 +13,8 @@ const rmb2Data = {
         { key: "size", label: "标准尺寸" },
         { key: "condition", label: "评级分数" },
         { key: "price", label: "购入价格" },
-        { key: "purchaseDate", label: "购入日期" }
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编码" }
     ],
 
     series: [
@@ -33,7 +34,8 @@ const rmb2Data = {
                     issueDate: "1955年3月1日",
                     withdrawnDate: "2007年4月1日",
                     wmk: "无水印/Without Watermark",
-                    size: "90×42.5mm",
+                    size: "90mm*42.5mm",
+                    krause: "860a",
                     remark: "三位罗马冠字+七位阿拉伯数字，有'儿'版和无'儿'版两种暗记",
                     img1: "image/rmb2/-1.jpg",
                     img2: "image/rmb2/-2.jpg"
@@ -55,7 +57,8 @@ const rmb2Data = {
                     issueDate: "1981年",
                     withdrawnDate: "2007年4月1日",
                     wmk: "无水印/Without Watermark",
-                    size: "90×42.5mm",
+                    size: "90mm*42.5mm",
+                    krause: "860b.1",
                     remark: "只有罗马冠字（三位或二位），无阿拉伯数字。按冠字大小分为小三冠(4mm)、大三冠(5mm)、大二冠。这一张为小三冠。",
                     img1: "image/rmb2/722-1.jpg",
                     img2: "image/rmb2/722-2.jpg"
@@ -70,7 +73,8 @@ const rmb2Data = {
                     issueDate: "1981年",
                     withdrawnDate: "2007年4月1日",
                     wmk: "无水印/Without Watermark",
-                    size: "90×42.5mm",
+                    size: "90mm*42.5mm",
+                    krause: "860b.2",
                     remark: "只有罗马冠字（三位或二位），无阿拉伯数字。按冠字大小分为小三冠(4mm)、大三冠(5mm)、大二冠。这一张为大三冠。",
                     img1: "image/rmb2/122-1.jpg",
                     img2: "image/rmb2/122-2.jpg"
@@ -85,7 +89,8 @@ const rmb2Data = {
                     issueDate: "1981年",
                     withdrawnDate: "2007年4月1日",
                     wmk: "无水印/Without Watermark",
-                    size: "90×42.5mm",
+                    size: "90mm*42.5mm",
+                    krause: "860c",
                     remark: "只有罗马冠字（三位或二位），无阿拉伯数字。按冠字大小分为小三冠(4mm)、大三冠(5mm)、大二冠。这一张为大二冠。",
                     img1: "image/rmb2/50-1.jpg",
                     img2: "image/rmb2/50-2.jpg"
@@ -109,7 +114,8 @@ const rmb2Data = {
                     issueDate: "1955年3月1日",
                     withdrawnDate: "2007年4月1日",
                     wmk: "无水印/Without Watermark",
-                    size: "95×45mm",
+                    size: "95mm*45mm",
+                    krause: "861a",
                     remark: "三位罗马冠字+七位阿拉伯数字。暗记：正面飞机头有白点；背面右侧藏文下有党徽图案；背面行名字左方有倒斜'古'",
                     img1: "image/rmb2/741-7004155-1.jpg",
                     img2: "image/rmb2/741-7004155-2.jpg"
@@ -125,13 +131,14 @@ const rmb2Data = {
                     year: 1953,
                     version: "ⅡⅠⅡ",
                     bank: "中国人民银行",
-                    condition: ""ACG66E,
+                    condition: "ACG66E",
                     price: "",
                     purchaseDate: "",
                     issueDate: "1981年",
                     withdrawnDate: "2007年4月1日",
                     wmk: "无水印/Without Watermark",
-                    size: "95×45mm",
+                    size: "95mm*45mm",
+                    krause: "861c",
                     remark: "三位罗马冠字，无阿拉伯数字。按冠字大小分为小三冠(4mm)和大三冠(5mm)：小三冠飞机头无白点，大三冠飞机头有白点。这一张为小三冠。",
                     img1: "image/rmb2/212-1.jpg",
                     img2: "image/rmb2/212-2.jpg"
@@ -146,7 +153,8 @@ const rmb2Data = {
                     issueDate: "1981年",
                     withdrawnDate: "2007年4月1日",
                     wmk: "无水印/Without Watermark",
-                    size: "95×45mm",
+                    size: "95mm*45mm",
+                    krause: "861b",
                     remark: "三位罗马冠字，无阿拉伯数字。按冠字大小分为小三冠(4mm)和大三冠(5mm)：小三冠飞机头无白点，大三冠飞机头有白点。这一张为小三冠。",
                     img1: "image/rmb2/155-1.jpg",
                     img2: "image/rmb2/155-2.jpg"
@@ -170,7 +178,8 @@ const rmb2Data = {
                     issueDate: "1955年3月1日",
                     withdrawnDate: "2007年4月1日",
                     wmk: "无水印/Without Watermark",
-                    size: "100×47.5mm",
+                    size: "100mm*47.5mm",
+                    krause: "862a",
                     remark: "三位罗马冠字+七位阿拉伯数字。暗记：正面轮船尾部有字母'P'和'H'；底纹偏浅草绿色",
                     img1: "image/rmb2/-1.jpg",
                     img2: "image/rmb2/-2.jpg"
@@ -186,13 +195,14 @@ const rmb2Data = {
                     year: 1953,
                     version: "ⅢⅤⅢ",
                     bank: "中国人民银行",
-                    condition: ""ACG67E,
+                    condition: "ACG67E",
                     price: "",
                     purchaseDate: "",
                     issueDate: "1981年",
                     withdrawnDate: "2007年4月1日",
                     wmk: "无水印/Without Watermark",
-                    size: "100×47.5mm",
+                    size: "100mm*47.5mm",
+                    krause: "862c",
                     remark: "三位罗马冠字，无阿拉伯数字。按冠字大小分为小三冠(4mm)和大三冠(5mm)；底纹为橄榄绿色。这一张为小三冠。",
                     img1: "image/rmb2/353-1.jpg",
                     img2: "image/rmb2/353-2.jpg"
@@ -207,7 +217,8 @@ const rmb2Data = {
                     issueDate: "1981年",
                     withdrawnDate: "2007年4月1日",
                     wmk: "无水印/Without Watermark",
-                    size: "100×47.5mm",
+                    size: "100mm*47.5mm",
+                    krause: "862b",
                     remark: "三位罗马冠字，无阿拉伯数字。按冠字大小分为小三冠(4mm)和大三冠(5mm)；底纹为橄榄绿色。这一张为小三冠。",
                     img1: "image/rmb2/055-1.jpg",
                     img2: "image/rmb2/055-2.jpg"
