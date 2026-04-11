245a2d2af4bc5319f9f8fb6760c8ecdbd0f04e60
Tong-Xiangjie <2011746785@qq.com>
2026-04-11 13:52:49 +0800

    Update republic_kpb.js

--- notecollection/data/republic_kpb.js
+++ notecollection/data/republic_kpb.js
@@ -2,6 +2,213 @@
 const republic_kpbData = {
     name: "民国广东省银行",
     icon: "𝟚𝟘",
-    desc: "民国广东省银行纸币",
-    series: []
+    desc: "民国广东省银行纸币·大洋票",
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "print", label: "印刷机构" },
+        { key: "year", label: "发行年份" },
+        { key: "issueDate", label: "发行日期" },
+        { key: "withdrawnDate", label: "停止流通日期" },
+        { key: "size", label: "标准尺寸" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编码" }
+    ],
+
+    series: [
+        // ==================== 壹分 ====================
+        {
+            seriesName: "1949年 1分 大洋票",
+            year: "1949",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1949,
+                    version: "AK",
+                    bank: "广东省银行/The Kuangtung Provincial Bank",
+                    print: "中华书局股份有限公司/CHB",
+                    issueDate: "1949年6月30日",
+                    withdrawnDate: "1949年10月12日",
+                    size: "95mm*45mm",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "S2452",
+                    remark: "辅币券，背面无图案。大洋票是中华民国政府在大陆地区发行的最后一套纸币，流通时间仅四个月零六天",
+                    img1: "image/republic_kpb/AK-1.jpg",
+                    img2: "image/republic_kpb/AK-2.jpg"
+                }
+            ]
+        },
+        // ==================== 伍分 ====================
+        {
+            seriesName: "1949年 5分 大洋票",
+            year: "1949",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1949,
+                    version: "AJ",
+                    bank: "广东省银行/The Kuangtung Provincial Bank",
+                    print: "中华书局股份有限公司/CHB",
+                    issueDate: "1949年6月30日",
+                    withdrawnDate: "1949年10月12日",
+                    size: "95mm*45mm",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "S2453",
+                    remark: "辅币券，背面无图案。凸版印刷，票面细节精细",
+                    img1: "image/republic_kpb/AJ-1.jpg",
+                    img2: "image/republic_kpb/AJ-2.jpg"
+                }
+            ]
+        },
+        // ==================== 壹角 ====================
+        {
+            seriesName: "1949年 1角 大洋票",
+            year: "1949",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1949,
+                    version: "AU338113",
+                    bank: "广东省银行/The Kuangtung Provincial Bank",
+                    print: "中华书局股份有限公司/CHB",
+                    issueDate: "1949年6月6日",
+                    withdrawnDate: "1949年10月12日",
+                    size: "100mm*48mm",
+                    condition: "ACG65E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "S2454",
+                    remark: "辅币券，背面图案为广州中山纪念堂",
+                    img1: "image/republic_kpb/AU338113-1.jpg",
+                    img2: "image/republic_kpb/AU338113-2.jpg"
+                }
+            ]
+        },
+        // ==================== 伍角 ====================
+        {
+            seriesName: "1949年 5角 大洋票",
+            year: "1949",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1949,
+                    version: "AB296519",
+                    bank: "广东省银行/The Kuangtung Provincial Bank",
+                    print: "中华书局股份有限公司/CHB",
+                    issueDate: "1949年6月6日",
+                    withdrawnDate: "1949年10月12日",
+                    size: "100mm*48mm",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "S2455",
+                    remark: "辅币券，背面图案为广州中山纪念堂",
+                    img1: "image/republic_kpb/AB296519-1.jpg",
+                    img2: "image/republic_kpb/AB296519-2.jpg"
+                }
+            ]
+        },
+        // ==================== 壹元 ====================
+        {
+            seriesName: "1949年 1元 大洋票",
+            year: "1949",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1949,
+                    version: "AS933975",
+                    bank: "广东省银行/The Kuangtung Provincial Bank",
+                    print: "中华书局股份有限公司/CHB",
+                    issueDate: "1949年7月",
+                    withdrawnDate: "1949年10月12日",
+                    size: "130mm*58mm",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "S2456",
+                    remark: "主币券，背面图案为广州中山纪念堂",
+                    img1: "image/republic_kpb/AS933975-1.jpg",
+                    img2: "image/republic_kpb/AS933975-2.jpg"
+                }
+            ]
+        },
+        // ==================== 伍元 ====================
+        {
+            seriesName: "1949年 5元 大洋票",
+            year: "1949",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1949,
+                    version: "AE184274",
+                    bank: "广东省银行/The Kuangtung Provincial Bank",
+                    print: "中华书局股份有限公司/CHB",
+                    issueDate: "未正式发行",
+                    withdrawnDate: "未正式发行",
+                    size: "140mm*60mm",
+                    condition: "ACG65E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "S2457",
+                    remark: "主币券，背面图案为广州中山纪念堂。伍元及以上面值因国民政府推行银元券，未正式发行，仅有样票或库存流入市场",
+                    img1: "image/republic_kpb/AE184274-1.jpg",
+                    img2: "image/republic_kpb/AE184274-2.jpg"
+                }
+            ]
+        },
+        // ==================== 拾元 ====================
+        {
+            seriesName: "1949年 10元 大洋票",
+            year: "1949",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1949,
+                    version: "AQ995712",
+                    bank: "广东省银行/The Kuangtung Provincial Bank",
+                    print: "中华书局股份有限公司/CHB",
+                    issueDate: "未正式发行",
+                    withdrawnDate: "未正式发行",
+                    size: "145mm*62mm",
+                    condition: "ACG65E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "S2458",
+                    remark: "主币券，背面图案为广州中山纪念堂。因国民政府推行银元券，未正式发行",
+                    img1: "image/republic_kpb/AQ995712-1.jpg",
+                    img2: "image/republic_kpb/AQ995712-2.jpg"
+                }
+            ]
+        },
+        // ==================== 壹佰元 ====================
+        {
+            seriesName: "1949年 100元 大洋票",
+            year: "1949",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1949,
+                    version: "AA147037",
+                    bank: "广东省银行/The Kuangtung Provincial Bank",
+                    print: "中华书局股份有限公司/CHB",
+                    issueDate: "未正式发行",
+                    withdrawnDate: "未正式发行",
+                    size: "148mm*60mm",
+                    condition: "ACG66E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "S2459",
+                    remark: "主币券，背面图案为广州中山纪念堂。雕刻版印制，票面孙中山像发丝纤毫毕现。因国民政府推行银元券，未正式发行",
+                    img1: "image/republic_kpb/AA147037-1.jpg",
+                    img2: "image/republic_kpb/AA147037-2.jpg"
+                }
+            ]
+        }
+    ]
 };
