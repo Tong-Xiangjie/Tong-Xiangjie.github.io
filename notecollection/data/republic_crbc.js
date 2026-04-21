86a521583a15ea221b25e6ac5eef75e85e4e486d
Tong-Xiangjie <2011746785@qq.com>
2026-04-21 22:05:39 +0800

    Update republic_crbc.js

--- notecollection/data/republic_crbc.js
+++ notecollection/data/republic_crbc.js
@@ -1,7 +1,7 @@
 // data/republic_crbc.js
 const republic_crbcData = {
     name: "民国中央储备银行",
-    icon: "𝟚𝟙",
-    desc: "民国中央储备银行纸币",
+    icon: null,
+    desc: "The Central Reserve Bank of China",
     series: []
 };
