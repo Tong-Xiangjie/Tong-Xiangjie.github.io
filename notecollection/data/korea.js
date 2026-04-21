e5298ff9cae0bdc8539408e7ead6b78d99925a4c
Tong-Xiangjie <2011746785@qq.com>
2026-04-21 20:31:36 +0800

    更新 korea.js

--- notecollection/data/korea.js
+++ notecollection/data/korea.js
@@ -1,7 +1,82 @@
-// data/korea.js
+// data/russia.js
 const koreaData = {
-    name: "韩国",
-    icon: "𝟚𝟞",
-    desc: "韩国纸币",
-    series: []
+    name: "俄罗斯",
+    icon: null,
+    desc: "Russia",
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "year", label: "发行年份" },
+        { key: "watermark", label: "水印" },
+        { key: "size", label: "标准尺寸" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    series: [
+        {
+            seriesName: "2014年版 100卢布 索契冬奥会纪念钞",
+            year: "2014",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2014,
+                    version: "Aa1851144",
+                    bank: "俄罗斯银行/Bank of Russia",
+                    condition: "ACG68E",
+                    price: "47.5元",
+                    purchaseDate: "2025年12月6日",
+                    krause: "274c",
+                    size: "65mm*150mm",
+                    watermark: "滑雪运动员",
+                    remark: "索契冬奥会纪念钞，冠号Aa",
+                    img1: "image/russia/Aa1851144-1.jpg",
+                    img2: "image/russia/Aa1851144-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2015年版 100卢布 克里米亚回归纪念钞",
+            year: "2015",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2015,
+                    version: "KC9688098",
+                    bank: "俄罗斯银行/Bank of Russia",
+                    condition: "ACG67E",
+                    price: "88元",
+                    purchaseDate: "2015年12月6日",
+                    krause: "275b",
+                    size: "65mm*150mm",
+                    watermark: "克里米亚古迹/斯瓦洛格石",
+                    remark: "克里米亚回归纪念钞",
+                    img1: "image/russia/KC9688098-1.jpg",
+                    img2: "image/russia/KC9688098-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018年版 100卢布 世界杯纪念钞",
+            year: "2018",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2018,
+                    version: "AB005500088",
+                    bank: "俄罗斯银行/Bank of Russia",
+                    condition: "ACG67E",
+                    price: "128元",
+                    purchaseDate: "2025年12月6日",
+                    krause: "280",
+                    size: "65mm*150mm",
+                    watermark: "足球运动员/守门员",
+                    remark: "2018年世界杯纪念钞",
+                    img1: "image/russia/AB005500088-1.jpg",
+                    img2: "image/russia/AB005500088-2.jpg"
+                }
+            ]
+        }
+    ]
 };
