f04fbacd5482759d1f9236b4f9229902487a895f
Tong-Xiangjie <2011746785@qq.com>
2026-04-21 20:44:15 +0800

    更新 indonesia.js

--- notecollection/data/indonesia.js
+++ notecollection/data/indonesia.js
@@ -1,7 +1,103 @@
 // data/indonesia.js
 const indonesiaData = {
-    name: "印尼",
-    icon: "𝟚𝟛",
-    desc: "印尼纸币",
-    series: []
+    name: "印度尼西亚",
+    icon: null,
+    desc: "Indonesia",
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "year", label: "发行年份" },
+        { key: "watermark", label: "水印" },
+        { key: "size", label: "标准尺寸" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    series: [
+        {
+            seriesName: "2000年版 1000卢比",
+            year: "2016",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2016,
+                    version: "FPK115111",
+                    bank: "印度尼西亚银行/Bank Indonesia",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "141n",
+                    size: "141 × 67 mm",
+                    watermark: "Tjut Njak Dien",
+                    remark: "豹子号（数二 金马号 全奇王）",
+                    img1: "image/indonesia/FPK115111-1.jpg",
+                    img2: "image/indonesia/FPK115111-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2016年版 2000卢比",
+            year: "2017",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2017,
+                    version: "FBK599997",
+                    bank: "印度尼西亚银行/Bank Indonesia",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "155b",
+                    size: "141 × 67 mm",
+                    watermark: "Mohammad Hoesni Thamrin",
+                    remark: "狮子身（数三 飞马王 全奇王）",
+                    img1: "image/indonesia/FBK599997-1.jpg",
+                    img2: "image/indonesia/FBK599997-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2016年版 100000卢比",
+            year: "2024",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2024,
+                    version: "ORE322444",
+                    bank: "印度尼西亚银行/Bank Indonesia",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "160g",
+                    size: "151 × 66 mm",
+                    watermark: "Soekarno & Mohammad Hatta",
+                    remark: "豹子号（数三 葫芦号 全小号）",
+                    img1: "image/indonesia/ORE322444-1.jpg",
+                    img2: "image/indonesia/ORE322444-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2020年独立75周年纪念版 75000卢比",
+            year: "2020",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2020,
+                    version: "AAA829876",
+                    bank: "印度尼西亚银行/Bank Indonesia",
+                    condition: "ACG67E",
+                    price: "",
+                    purchaseDate: "",
+                    krause: "161",
+                    size: "150 × 65 mm",
+                    watermark: "Soekarno & Mohammad Hatta",
+                    remark: "纪念独立75周年（Commemorative）",
+                    img1: "image/indonesia/AAA829876-1.jpg",
+                    img2: "image/indonesia/AAA829876-2.jpg"
+                }
+            ]
+        }
+    ]
 };
