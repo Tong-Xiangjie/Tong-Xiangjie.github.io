2007ec8a12c19d2f85d7bc84a4669c4407ad430b
Tong-Xiangjie <2011746785@qq.com>
2026-04-21 20:29:21 +0800

    更新 ukarine.js

--- notecollection/data/ukarine.js
+++ notecollection/data/ukarine.js
@@ -1,12 +1,13 @@
-// data/ukarine.js
-const ukarineData = {
-    name: "乌克兰",
+// data/russia.js
+const russiaData = {
+    name: "俄罗斯",
     icon: null,
-    desc: "Ukraine",
+    desc: "Russia",
     detailFields: [
         { key: "version", label: "冠字号码" },
         { key: "bank", label: "发行银行" },
         { key: "year", label: "发行年份" },
+        { key: "watermark", label: "水印" },
         { key: "size", label: "标准尺寸" },
         { key: "condition", label: "评级分数" },
         { key: "price", label: "购入价格" },
@@ -15,42 +16,65 @@ const ukarineData = {
     ],
     series: [
         {
-            seriesName: "2023年版 20格里夫纳 俄乌战争1周年纪念钞",
-            year: "2023",
+            seriesName: "2014年版 100卢布 索契冬奥会纪念钞",
+            year: "2014",
             copies: [
                 {
                     copyId: 1,
-                    year: 2023,
-                    version: "ЗС0230855",
-                    bank: "乌克兰国家银行/National Bank of Ukraine",
-                    condition: "ACG67E",
-                    price: "158元",
-                    purchaseDate: "2026年1月29日",
-                    krause: "133",
-                    size: "80mm*165mm",
-                    remark: "发行量30万张。",
-                    img1: "image/ukarine/0230855-1.jpg",
-                    img2: "image/ukarine/0230855-2.jpg"
+                    year: 2014,
+                    version: "Aa1851144",
+                    bank: "俄罗斯银行/Bank of Russia",
+                    condition: "ACG68E",
+                    price: "47.5元",
+                    purchaseDate: "2025年12月6日",
+                    krause: "274c",
+                    size: "65mm*150mm",
+                    watermark: "滑雪运动员",
+                    remark: "索契冬奥会纪念钞，冠号Aa",
+                    img1: "image/russia/Aa1851144-1.jpg",
+                    img2: "image/russia/Aa1851144-2.jpg"
                 }
             ]
         },
         {
-            seriesName: "2024年版 50格里夫纳 俄乌战争2周年纪念钞",
-            year: "2024",
+            seriesName: "2015年版 100卢布 克里米亚回归纪念钞",
+            year: "2015",
             copies: [
                 {
                     copyId: 1,
-                    year: 2024,
-                    version: "ЄС0244876",
-                    bank: "乌克兰国家银行/National Bank of Ukraine",
-                    condition: "ACG68E",
-                    price: "132元",
-                    purchaseDate: "2026年3月1日",
-                    krause: "134",
-                    size: "80mm*165mm",
-                    remark: "发行量30万张。",
-                    img1: "image/ukarine/0244876-1.jpg",
-                    img2: "image/ukarine/0244876-2.jpg"
+                    year: 2015,
+                    version: "KC9688098",
+                    bank: "俄罗斯银行/Bank of Russia",
+                    condition: "ACG67E",
+                    price: "88元",
+                    purchaseDate: "2015年12月6日",
+                    krause: "275b",
+                    size: "65mm*150mm",
+                    watermark: "克里米亚古迹/斯瓦洛格石",
+                    remark: "克里米亚回归纪念钞",
+                    img1: "image/russia/KC9688098-1.jpg",
+                    img2: "image/russia/KC9688098-2.jpg"
+                }
+            ]
+        },
+        {
+            seriesName: "2018年版 100卢布 世界杯纪念钞",
+            year: "2018",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2018,
+                    version: "AB005500088",
+                    bank: "俄罗斯银行/Bank of Russia",
+                    condition: "ACG67E",
+                    price: "128元",
+                    purchaseDate: "2025年12月6日",
+                    krause: "280",
+                    size: "65mm*150mm",
+                    watermark: "足球运动员/守门员",
+                    remark: "2018年世界杯纪念钞",
+                    img1: "image/russia/AB005500088-1.jpg",
+                    img2: "image/russia/AB005500088-2.jpg"
                 }
             ]
         }
