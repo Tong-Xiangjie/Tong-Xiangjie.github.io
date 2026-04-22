8dd7ccfd4cbaba1cd5b185831ebd072f82f699ca
Tong-Xiangjie <2011746785@qq.com>
2026-04-22 10:22:37 +0800

    更新 hk_hsbc.js

--- notecollection/data/hk_hsbc.js
+++ notecollection/data/hk_hsbc.js
@@ -1,7 +1,7 @@
 // data/hk_hsbc.js
 const hk_hsbcData = {
     name: "香港汇丰银行",
-    icon: "𝟘𝟛",
+    icon: null,
     desc: "The Hong Kong and Shanghai Banking Corporation",
     // 港币板块的详情页字段配置
     detailFields: [
