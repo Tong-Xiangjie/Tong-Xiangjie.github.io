bee813ca709c638032582c3c727bf1873f0cfc81
Tong-Xiangjie <2011746785@qq.com>
2026-05-02 22:22:16 +0800

    Create data.js

---
+++ funcollection/years/data.js
@@ -0,0 +1,20 @@
+// 年份图鉴数据
+// 每张纸币只需要：年份、名称、克劳斯编号、年份实拍图路径
+const banknoteYears = [
+    { year: 1979, name: "外汇兑换券 1角 (五星火炬)", krause: "FX1s", yearImg: "../../coincollection/image/fec/year/1979-1.jpg" },
+    { year: 1979, name: "外汇兑换券 1角 (五星)", krause: "FX1a", yearImg: "../../coincollection/image/fec/year/1979-2.jpg" },
+    { year: 1979, name: "外汇兑换券 5角", krause: "FX2", yearImg: "../../coincollection/image/fec/year/1979-3.jpg" },
+    { year: 1979, name: "外汇兑换券 1元", krause: "FX3", yearImg: "../../coincollection/image/fec/year/1979-4.jpg" },
+    { year: 1979, name: "外汇兑换券 5元", krause: "FX4", yearImg: "../../coincollection/image/fec/year/1979-5.jpg" },
+    { year: 1979, name: "外汇兑换券 10元", krause: "FX5s", yearImg: "../../coincollection/image/fec/year/1979-6.jpg" },
+    { year: 1979, name: "外汇兑换券 50元", krause: "FX6s", yearImg: "../../coincollection/image/fec/year/1979-7.jpg" },
+    { year: 1979, name: "外汇兑换券 100元", krause: "FX7s", yearImg: "../../coincollection/image/fec/year/1979-8.jpg" },
+    { year: 1953, name: "第二套人民币 1分", krause: "Pick# 1", yearImg: "../../coincollection/image/rmb2/year/1953-1.jpg" },
+    { year: 1953, name: "第二套人民币 2分", krause: "Pick# 2", yearImg: "../../coincollection/image/rmb2/year/1953-2.jpg" },
+    { year: 1953, name: "第二套人民币 5分", krause: "Pick# 3", yearImg: "../../coincollection/image/rmb2/year/1953-3.jpg" },
+    { year: 1980, name: "第四套人民币 1角", krause: "Pick# 4", yearImg: "../../coincollection/image/rmb4/year/1980-1.jpg" },
+    { year: 1990, name: "第四套人民币 1元", krause: "Pick# 5", yearImg: "../../coincollection/image/rmb4/year/1990-1.jpg" },
+    { year: 2005, name: "第五套人民币 10元", krause: "Pick# 6", yearImg: "../../coincollection/image/rmb5/year/2005-1.jpg" },
+    { year: 1960, name: "第三套人民币 1元", krause: "Pick# 7", yearImg: "../../coincollection/image/rmb3/year/1960-1.jpg" },
+    { year: 1965, name: "第三套人民币 10元", krause: "Pick# 8", yearImg: "../../coincollection/image/rmb3/year/1965-1.jpg" }
+];
