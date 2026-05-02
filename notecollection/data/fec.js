d923434b8b4f6899454a54055f50fb94a5fd703d
Tong-Xiangjie <2011746785@qq.com>
2026-05-02 18:40:25 +0800

    Update fec.js

--- notecollection/data/fec.js
+++ notecollection/data/fec.js
@@ -16,56 +16,60 @@ const fecData = {
     ],
     series: [
         {
-            seriesName: "1979年 1角 五星火炬水印",
+            seriesName: "1角",
             year: "1979",
-            copies: [
+            varieties: [
                 {
-                    copyId: 1,
-                    year: 1979,
-                    purchaseDate: "年月日",
-                    price: "元",
-                    bank: "中国银行",
-                    version: "Undefined",
-                    condition: "Undefined00E",
-                    krause: "FX1b",
-                    wmk: "五星火炬/Star&Torch",
-                    remark: "占位，暂无藏品",
-                    img1: "",
-                    img2: ""
-                }
-            ]
-        },
-        {
-            seriesName: "1979年 1角 五星水印",
-            year: "1979",
-            copies: [
-                {
-                    copyId: 1,
-                    year: 1979,
-                    purchaseDate: "2025年10月20日",
-                    price: "76元",
-                    bank: "中国银行",
-                    version: "AI172872",
-                    condition: "ACG67E",
-                    krause: "FX1a",
-                    wmk: "五星/Stars",
-                    remark: "无荧光",
-                    img1: "image/fec/AI172872-1.jpg",
-                    img2: "image/fec/AI172872-2.jpg"
+                    varietyName: "五星火炬水印",
+                    copies: [
+                        {
+                            copyId: 1,
+                            year: 1979,
+                            purchaseDate: "2026年5月2日",
+                            price: "280元",
+                            bank: "中国银行",
+                            version: "ZA000000 17064",
+                            condition: "ACG65E",
+                            krause: "FX1s",
+                            wmk: "五星火炬/Star&Torch",
+                            remark: "在孔网上面捡漏！",
+                            img1: "image/fec/17064-1.jpg",
+                            img2: "image/fec/17064-2.jpg"
+                        }
+                    ]
                 },
                 {
-                    copyId: 2,
-                    year: 1979,
-                    purchaseDate: "2025年12月31日",
-                    price: "66元",
-                    bank: "中国银行",
-                    version: "DL832836",
-                    condition: "ACG66E",
-                    krause: "FX1a",
-                    wmk: "五星/Stars",
-                    remark: "有荧光",
-                    img1: "image/fec/DL832836-1.jpg",
-                    img2: "image/fec/DL832836-2.jpg"
+                    varietyName: "五星水印",
+                    copies: [
+                        {
+                            copyId: 1,
+                            year: 1979,
+                            purchaseDate: "2025年10月20日",
+                            price: "76元",
+                            bank: "中国银行",
+                            version: "AI172872",
+                            condition: "ACG67E",
+                            krause: "FX1a",
+                            wmk: "五星/Stars",
+                            remark: "无荧光",
+                            img1: "image/fec/AI172872-1.jpg",
+                            img2: "image/fec/AI172872-2.jpg"
+                        },
+                        {
+                            copyId: 2,
+                            year: 1979,
+                            purchaseDate: "2025年12月31日",
+                            price: "66元",
+                            bank: "中国银行",
+                            version: "DL832836",
+                            condition: "ACG66E",
+                            krause: "FX1a",
+                            wmk: "五星/Stars",
+                            remark: "有荧光",
+                            img1: "image/fec/DL832836-1.jpg",
+                            img2: "image/fec/DL832836-2.jpg"
+                        }
+                    ]
                 }
             ]
         },
