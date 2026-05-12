513f98d65949addbe54d52e16ecbf8945e34431c
Tong-Xiangjie <2011746785@qq.com>
2026-05-13 00:05:45 +0800

    Update indonesia.js

--- notecollection/data/indonesia.js
+++ notecollection/data/indonesia.js
@@ -77,6 +77,26 @@ const indonesiaData = {
                     img2: "image/indonesia/ORE322444-2.jpg"
                 }
             ]
+        },{
+            seriesName: "2022年版 1000卢比",
+            year: "2022",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 2024,
+                    version: "KAO088999",
+                    bank: "印度尼西亚银行/Bank Indonesia",
+                    condition: "ACG66E",
+                    price: "76元",
+                    purchaseDate: "2025年5月12日",
+                    krause: "162a",
+                    size: "",
+                    watermark: "",
+                    remark: "这张爱藏标签信息错了^_^，把第三个冠号O当作了数字0，因此标签上面写的是KA0088999，这还使得这张钞票多了一个它本来不该有的版别“连对号”，这也有点搞笑了。不过这一张我是一眼就看上的，所以一开始看到的时候拍卖出价还是3元的时候我就下定决心100以内都可以接受（虽然后来看到出价到了73还是有点肉疼ToT），不过最好还是咬咬牙买到哩~这张的数字冠O是我看上它的最大亮点，况且后面就马上紧邻一个数字0，可以很好地对比这两个字在字体上的差别！",
+                    img1: "image/indonesia/ORE322444-1.jpg",
+                    img2: "image/indonesia/ORE322444-2.jpg"
+                }
+            ]
         },
         {
             seriesName: "2020年独立75周年纪念版 75000卢比",
