4b7bd0d04796fc04c0aceed1b8c6731a2bf6d62a
Tong-Xiangjie <2011746785@qq.com>
2026-06-06 13:42:23 +0800

    Create japan_military.js

---
+++ notecollection/data/japan_military.js
@@ -0,0 +1,100 @@
+const japanMilitaryData = {
+    name: "日本帝国政府军用手票",
+    icon: null,
+    desc: "Japanese Imperial Government Military Note",
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行单位" },
+        { key: "print", label: "印刷地区" },
+        { key: "year", label: "发行年份" },
+        { key: "issueDate", label: "发行日期" },
+        { key: "withdrawnDate", label: "退出流通" },
+        { key: "size", label: "标准尺寸" },
+        { key: "wmk", label: "水印" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯编号" }
+    ],
+
+    series: [
+        // 甲号【1937，无百元，只有小面额：10钱/50钱/1/5/10圆】
+        {
+            seriesName: "甲号券",
+            year: "1937",
+            varieties: [
+                { varietyName: "1937年 10钱 甲号军用手票", copies: [] },
+                { varietyName: "1937年 50钱 甲号军用手票", copies: [] },
+                { varietyName: "1937年 1圆 甲号军用手票", copies: [] },
+                { varietyName: "1937年 5圆 甲号军用手票", copies: [] },
+                { varietyName: "1937年 10圆 甲号军用手票", copies: [] }
+            ]
+        },
+
+        // 乙号【1938，5/10/100圆；原版改票、涂黑日本银行、本土凤凰水印】
+        {
+            seriesName: "乙号券",
+            year: "1938",
+            varieties: [
+                { varietyName: "1938年 5圆 乙号军用手票（本土水印版）", copies: [] },
+                { varietyName: "1938年 10圆 乙号军用手票（本土水印版）", copies: [] },
+                { varietyName: "1938年 100圆 乙号军用手票（本土水印版）", copies: [] }
+            ]
+        },
+
+        // 丙号【全套：1/5/10/100圆，分：日本本土水印版、沦陷区现地无水印版（你藏品在此）】
+        {
+            seriesName: "丙号券",
+            year: "1938",
+            varieties: [
+                { varietyName: "1938年 1圆 丙号军用手票", copies: [] },
+                { varietyName: "1938年 5圆 丙号军用手票", copies: [] },
+                { varietyName: "1938年 10圆 丙号军用手票", copies: [] },
+                { varietyName: "1938年 100圆 丙号券", copies: [
+                  {
+                            copyId: 1,
+                            year: 1938,
+                            version: "1",
+                            bank: "日本帝国政府",
+                            condition: "ACG65E",
+                            price: "78元",
+                            purchaseDate: "2026年6月6日",
+                            krause: "M29",
+                            print: "日本帝国印刷局",
+                            issueDate: "",
+                            withdrawnDate: "",
+                            size: "",
+                            wmk: "",
+                            remark: "",
+                            img1: "image/japan/1-1.jpg",
+                            img2: "image/japan/1-2.jpg"
+                        }
+                ] }
+            ]
+        },
+
+        // 丁号【龙凤无圣德人像：1/5/10/100圆】
+        {
+            seriesName: "丁号券",
+            year: "1939",
+            varieties: [
+                { varietyName: "1939年 1圆 丁号龙凤军用手票", copies: [] },
+                { varietyName: "1939年 5圆 丁号龙凤军用手票", copies: [] },
+                { varietyName: "1939年 10圆 丁号龙凤军用手票", copies: [] },
+                { varietyName: "1939年 100圆 丁号龙凤军用手票", copies: [] }
+            ]
+        },
+
+        // 戊号【无红字“军用手票”、单凤：1/5/10/100圆】
+        {
+            seriesName: "戊号券",
+            year: "1939",
+            varieties: [
+                { varietyName: "1939年 1圆 戊号军用手票", copies: [] },
+                { varietyName: "1939年 5圆 戊号军用手票", copies: [] },
+                { varietyName: "1939年 10圆 戊号军用手票", copies: [] },
+                { varietyName: "1939年 100圆 戊号军用手票", copies: [] }
+            ]
+        }
+    ]
+};
