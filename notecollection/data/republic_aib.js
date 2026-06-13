2c8adb9eaf40d39e90129063c3d23a282c03bcf5
Tong-Xiangjie <2011746785@qq.com>
2026-06-14 00:10:41 +0800

    Create republic_aib.js

---
+++ notecollection/data/republic_aib.js
@@ -0,0 +1,44 @@
+// data/republic_aib.js
+const republic_aibData = {
+    name: "民国厦门劝业银行",
+    icon: null,
+    desc: "The Amoy Industrial Bank",
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "print", label: "印刷机构" },
+        { key: "year", label: "发行年份" },
+        { key: "issueDate", label: "发行日期" },
+        { key: "withdrawnDate", label: "停止流通日期" },
+        { key: "size", label: "标准尺寸" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编码" }
+    ],
+    series: [
+        {
+            seriesName: "1940年 1分",
+            year: "1940",
+            copies: [
+                {
+                    copyId: 1,
+                    year: 1940,
+                    version: "无字冠",
+                    bank: "厦门劝业银行",
+                    print: "",
+                    issueDate: "",
+                    withdrawnDate: "",
+                    size: "",
+                    condition: "ACG66E",
+                    price: "28元",
+                    purchaseDate: "2026年6月11日",
+                    krause: "S1655",
+                    remark: "",
+                    img1: "image/republic_aib/1940-0.01-1.jpg",
+                    img2: "image/republic_aib/1940-0.01-2.jpg"
+                }
+            ]
+        }
+    ]
+};
