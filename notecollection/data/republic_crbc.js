ecc89f9fcdac455099699f5967d9b0e3026ee6f4
Tong-Xiangjie <2011746785@qq.com>
2026-06-17 10:45:47 +0800

    Update republic_crbc.js

--- notecollection/data/republic_crbc.js
+++ notecollection/data/republic_crbc.js
@@ -3,5 +3,35 @@ const republic_crbcData = {
     name: "民国中央储备银行",
     icon: null,
     desc: "The Central Reserve Bank of China",
-    series: []
+    series: 
+    [
+        {
+            seriesName: "占位用",
+            year: "0000",
+            varieties: [
+                {
+                    varietyName: "0",
+                    copies: [
+                        {
+                            copyId: 1,
+                            year: 1939,
+                            version: "Y619524L",
+                            bank: "中央银行",
+                            print: "中华书局有限公司/CHB",
+                            issueDate: "民国二十年四月（1931）",
+                            withdrawnDate: "",
+                            size: "",
+                            condition: "ACGE",
+                            price: "元",
+                            purchaseDate: "2026年6月17日",
+                            krause: "202",
+                            remark: "",
+                            img1: "image/republic_crbc/Y619524L-1.jpg",
+                            img2: "image/republic_crbc/Y619524L-2.jpg"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 };
