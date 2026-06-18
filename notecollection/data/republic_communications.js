c2aaf31abe9bb734ff72c86be4f759c6595337db
Tong-Xiangjie <2011746785@qq.com>
2026-06-18 18:36:02 +0800

    Update republic_communications.js

--- notecollection/data/republic_communications.js
+++ notecollection/data/republic_communications.js
@@ -16,50 +16,53 @@ const republic_communicationsData = {
         { key: "purchaseDate", label: "购入日期" },
         { key: "krause", label: "克劳斯目录编码" }
     ],
-    series: [
+series: [
         {
-            seriesName: "1914年 5元 橄色火车头 加盖黑上海",
+            seriesName: "中华民国三年（1914年） 美钞版国币券",
             year: "1914",
-            copies: [
+            varieties: [
                 {
-                    copyId: 1,
-                    year: 1914,
-                    version: "SB648728R",
-                    bank: "交通银行",
-                    print: "美国钞票公司/ABNC",
-                    issueDate: "",
-                    withdrawnDate: "",
-                    size: "79mm*162mm",
-                    condition: "ACG64E",
-                    price: "188元",
-                    purchaseDate: "2026年2月4日",
-                    krause: "117n",
-                    remark: "",
-                    img1: "image/republic_communications/SB648728R-1.jpg",
-                    img2: "image/republic_communications/SB648728R-2.jpg"
-                }
-            ]
-        },
-        {
-            seriesName: "1914年 10元 红色大楼 加盖蓝上海",
-            year: "1914",
-            copies: [
-                {
-                    copyId: 1,
-                    year: 1914,
-                    version: "SB052887D",
-                    bank: "交通银行",
-                    condition: "ACG64E",
-                    price: "198元",
-                    purchaseDate: "2026年2月15日",
-                    krause: "118q",
-                    print: "美国钞票公司/ABNC",
-                    issueDate: "",
-                    withdrawnDate: "",
-                    size: "79mm*162mm",
-                    remark: "",
-                    img1: "image/republic_communications/SB052887D-1.jpg",
-                    img2: "image/republic_communications/SB052887D-2.jpg"
+                    varietyName: "1914年 5元 火车头（橄色） 加盖黑上海",
+                    copies: [
+                        {
+                            copyId: 1,
+                            year: 1914,
+                            version: "SB648728R",
+                            bank: "交通银行",
+                            print: "美国钞票公司/ABNC",
+                            issueDate: "",
+                            withdrawnDate: "",
+                            size: "79mm*162mm",
+                            condition: "ACG64E",
+                            price: "188元",
+                            purchaseDate: "2026年2月4日",
+                            krause: "117n",
+                            remark: "",
+                            img1: "image/republic_communications/SB648728R-1.jpg",
+                            img2: "image/republic_communications/SB648728R-2.jpg"
+                        }
+                    ]
+                },{
+                    varietyName: "1914年 10元 大楼（红色） 加盖蓝上海",
+                    copies: [
+                        {
+                            copyId: 1,
+                            year: 1914,
+                            version: "SB052887D",
+                            bank: "交通银行",
+                            condition: "ACG64E",
+                            price: "198元",
+                            purchaseDate: "2026年2月15日",
+                            krause: "118q",
+                            print: "美国钞票公司/ABNC",
+                            issueDate: "",
+                            withdrawnDate: "",
+                            size: "79mm*162mm",
+                            remark: "",
+                            img1: "image/republic_communications/SB052887D-1.jpg",
+                            img2: "image/republic_communications/SB052887D-2.jpg"
+                        }
+                    ]
                 }
             ]
         }
