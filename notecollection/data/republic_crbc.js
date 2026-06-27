b5dd564b1ebee140813296f20c09a4017a2c132e
Tong-Xiangjie <2011746785@qq.com>
2026-06-27 14:31:44 +0800

    Modify remark in republic_crbc.js
    Updated the copy's remark for better context.

--- notecollection/data/republic_crbc.js
+++ notecollection/data/republic_crbc.js
@@ -25,7 +25,7 @@ const republic_crbcData = {
                             price: "98元",
                             purchaseDate: "2026年6月27日",
                             krause: "J3a",
-                            remark: "",
+                            remark: "也许是特别的缘分，这是我买的第一张中央储备银行的纸币，一开始用中央银行的一张一角纸币（1931年杏坛）来占位，那张纸币的号码是619524，结果这一张号码刚好也是由这6个数字组成的，二者面值还一样，背面设计也是如此相似，真的算得上是双胞胎了。并且我是拍下后才发现这个关系的。",
                             img1: "image/republic_crbc/S165294D-1.jpg",
                             img2: "image/republic_crbc/S165294D-2.jpg"
                         }
