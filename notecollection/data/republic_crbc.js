fd9e6ed28bba3b0e05236e4100e5a92172eab998
Tong-Xiangjie <2011746785@qq.com>
2026-06-27 14:29:09 +0800

    Update republic_crbc.js with new bank data

--- notecollection/data/republic_crbc.js
+++ notecollection/data/republic_crbc.js
@@ -6,28 +6,28 @@ const republic_crbcData = {
     series: 
     [
         {
-            seriesName: "占位用",
-            year: "0000",
+            seriesName: "中华民国二十九年（1940年） 华成版 中储券",
+            year: "1940",
             varieties: [
                 {
-                    varietyName: "0",
+                    varietyName: "1940年 1角 南京中山陵",
                     copies: [
                         {
                             copyId: 1,
-                            year: 1939,
-                            version: "Y619524L",
-                            bank: "中央银行",
-                            print: "中华书局有限公司/CHB",
-                            issueDate: "民国二十年四月（1931）",
+                            year: 1940,
+                            version: "S165294D",
+                            bank: "中央储备银行",
+                            print: "华成印刷有限公司/WPC",
+                            issueDate: "民国二十九年（1940年）",
                             withdrawnDate: "",
                             size: "",
-                            condition: "ACGE",
-                            price: "元",
-                            purchaseDate: "2026年6月17日",
-                            krause: "202",
+                            condition: "ACG62E",
+                            price: "98元",
+                            purchaseDate: "2026年6月27日",
+                            krause: "J3a",
                             remark: "",
-                            img1: "image/republic_crbc/Y619524L-1.jpg",
-                            img2: "image/republic_crbc/Y619524L-2.jpg"
+                            img1: "image/republic_crbc/S165294D-1.jpg",
+                            img2: "image/republic_crbc/S165294D-2.jpg"
                         }
                     ]
                 }
