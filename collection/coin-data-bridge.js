6b9553af47b5be3c8b28a02f33c1a04e18dd1ca1
Tong-Xiangjie <2011746785@qq.com>
2026-07-06 19:01:54 +0800

    Create coin-data-bridge.js

---
+++ collection/coin-data-bridge.js
@@ -0,0 +1,6 @@
+// coin-data-bridge.js
+window.COIN_DATA_MAP = {
+    commemorativeData: typeof coincommData !== 'undefined' ? coincommData : null,
+    circulatingData:   typeof circulatingData !== 'undefined' ? circulatingData : null,
+    gold_silverData:   typeof gold_silverData !== 'undefined' ? gold_silverData : null
+};
