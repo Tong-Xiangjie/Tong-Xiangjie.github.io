d51fed7e54d02f2ec1da77a23e196d12403bfde4
Tong-Xiangjie <2011746785@qq.com>
2026-07-15 12:34:14 +0800

    Add readme section to republic_spb.js
    Added readme section with title and content reference.

--- notecollection/data/republic_spb.js
+++ notecollection/data/republic_spb.js
@@ -16,56 +16,11 @@ const republic_spbData = {
         { key: "purchaseDate", label: "购入日期" },
         { key: "krause", label: "克劳斯目录编码" }
     ],
-
+    readme: {
+        title: "买到低仿假币，遗憾离场",
+        content: "file:readmes/spb_fake_banknote.txt"
+    },
     series: [
-        {
-            seriesName: "中华民国三十八年（1949年） 区票",
-            year: "1949",
-            varieties: [
-                {
-                    varietyName: "1949年 10元",
-                    copies: [
-                        {
-                            copyId: 1,
-                            year: 1949,
-                            version: "AN097206",
-                            bank: "南方人民银行/Southern People's Bank",
-                            print: "未知",
-                            issueDate: "",
-                            withdrawnDate: "",
-                            size: "",
-                            condition: "暂未评级",
-                            price: "244元",
-                            purchaseDate: "2026年7月10日",
-                            krause: "S3489",
-                            remark: "",
-                            img1: "image/republic_spb/AN097206-1.jpg",
-                            img2: "image/republic_spb/AN097206-2.jpg"
-                        }
-                    ]
-                },{
-                    varietyName: "1949年 10元 改作闽粤赣边区银行（Fujian-Guangdong-Jiangxi Border District Bank）",
-                    copies: [
-                        {
-                            copyId: 1,
-                            year: 1949,
-                            version: "BN332503",
-                            bank: "闽粤赣边区银行/Fujian-Guangdong-Jiangxi Border District Bank",
-                            print: "未知",
-                            issueDate: "",
-                            withdrawnDate: "",
-                            size: "",
-                            condition: "暂未评级",
-                            price: "244元",
-                            purchaseDate: "2026年7月10日",
-                            krause: "S3482",
-                            remark: "",
-                            img1: "image/republic_spb/BN332503-1.jpg",
-                            img2: "image/republic_spb/BN332503-2.jpg"
-                        }
-                    ]
-                }
-            ]
-        }
+        
     ]
 };
