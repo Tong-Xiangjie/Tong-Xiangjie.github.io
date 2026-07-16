701250aea248736bcf34447d39f5b0fde7d84a7e
Tong-Xiangjie <2011746785@qq.com>
2026-07-16 20:50:59 +0800

    Create dscc.js

---
+++ notecollection/data/dscc.js
@@ -0,0 +1,22 @@
+// data/dscc.js
+const dsccData = {
+    name: "复员军人兑取现金券",
+    icon: null,
+    desc: "Demobilized Soldiers Cash Coupon",
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行部门" },
+        { key: "year", label: "发行年份" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    readme: {
+        title: "兑取规则",
+        content: "file:readmes/dscc_ordinance.txt"
+    },
+    series: [
+      
+    ]
+};
