3d037de4b2776bd7e6bb6fbabd3bbee8100f06c0
Tong-Xiangjie <2011746785@qq.com>
2026-07-16 20:43:37 +0800

    Create pvpb.js

---
+++ notecollection/data/pvpb.js
@@ -0,0 +1,22 @@
+// data/pvpb.js
+const pvpbData = {
+    name: "人民胜利折实公债券",
+    icon: null,
+    desc: "The People's Victory Parity Bond",
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行部门" },
+        { key: "year", label: "发行年份" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编号" }
+    ],
+    readme: {
+        title: "一九五〇年第一期人民胜利折实公债条例",
+        content: "file:readmes/pvpb_ordinance.txt"
+    },
+    series: [
+      
+    ]
+};
