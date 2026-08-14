eeb79761828233088e9488bc07b8a2d1a07693c0
Tong-Xiangjie <2011746785@qq.com>
2026-08-14 15:09:48 +0800

    Update coin-config.js

--- collection/coin-config.js
+++ collection/coin-config.js
@@ -4,18 +4,21 @@ const coinCategoryTree = [
         id: 'commemorative_coins',
         name: '纪念币',
         dataKey: 'commemorativeData',
+        dataFile: '../coincollection/data/commemorative_coins.js',
         children: null
     },
     {
         id: 'circulating_coins',
         name: '流通币',
         dataKey: 'circulatingData',
+        dataFile: '../coincollection/data/circulating.js',
         children: null
     },
     {
         id: 'gold_silver_coins',
         name: '金银币',
         dataKey: 'gold_silverData',
+        dataFile: '../coincollection/data/gold_silver.js',
         children: null
     }
 ];
