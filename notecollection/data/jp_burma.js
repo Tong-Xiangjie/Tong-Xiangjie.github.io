39a7352a3f5e6dfb848f45ef35a074ae53214944
Tong-Xiangjie <2011746785@qq.com>
2026-08-15 00:55:31 +0800

    Create jp_burma.js

---
+++ notecollection/data/jp_burma.js
@@ -0,0 +1,45 @@
+// data/jp_burma.js
+const jp_burmaData = {
+    name: "日占缅甸",
+    icon: null,
+    desc: "Burma / Japanese Occupation WWⅡ",
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "print", label: "印刷机构" },
+        { key: "year", label: "发行年份" },
+        { key: "issueDate", label: "发行日期" },
+        { key: "withdrawnDate", label: "停止流通日期" },
+        { key: "size", label: "标准尺寸" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编码" }
+    ],
+
+  series: [
+        {
+            seriesName: "1944年 100卢比（Rupees）",
+            year: "1944",
+            copies: [
+                {
+                    copyId: 19968772,
+                    year: 1944,
+                    version: "BA",
+                    bank: "大日本帝国政府/The Japanese Imperial Government",
+                    print: "",
+                    issueDate: "",
+                    withdrawnDate: "",
+                    size: "",
+                    condition: "ACG66E",
+                    price: "58元",
+                    purchaseDate: "2026年8月14日",
+                    krause: "17b",
+                    remark: "版别：无水印，窄字母。草台班子爱藏把这张标成了日占菲律宾……",
+                    img1: "https://cdn.jsdelivr.net/gh/Tong-Xiangjie/Tong-Xiangjie.github.io@main/notecollection/image/jp_burma/1944-100-1.jpg",
+                    img2: "https://cdn.jsdelivr.net/gh/Tong-Xiangjie/Tong-Xiangjie.github.io@main/notecollection/image/jp_burma/1944-100-2.jpg"
+                }
+            ]
+        }
+    ]
+};
