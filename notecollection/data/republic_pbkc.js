8f06fa8d4662fd926693a433fcfbd9682b8fd9ed
Tong-Xiangjie <2011746785@qq.com>
2026-08-14 17:03:54 +0800

    Create republic_pbkc.js

---
+++ notecollection/data/republic_pbkc.js
@@ -0,0 +1,50 @@
+// data/republic_pbkc.js
+const republic_pbkcData = {
+    name: "民国贵州省银行",
+    icon: null,
+    desc: "The Provincial Bank of Kwuichow",
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "print", label: "印刷机构" },
+        { key: "year", label: "发行年份" },
+        { key: "issueDate", label: "发行日期" },
+        { key: "withdrawnDate", label: "停止流通日期" },
+        { key: "size", label: "标准尺寸" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编码" }
+    ],
+
+    series: [
+        {
+            seriesName: "中华民国三十八年（1949年） 中央重庆版 银元辅币券",
+            year: "1949",
+            varieties: [
+                {
+                    varietyName: "1949年 1分",
+                    copies: [
+                        {
+                            copyId: 10259025,
+                            year: 1949,
+                            version: "A338842",
+                            bank: "贵州省银行/The Provincial Bank of Kwuichow",
+                            print: "中央印制厂重庆厂/CPF",
+                            issueDate: "",
+                            withdrawnDate: "",
+                            size: "",
+                            condition: "ACG66E",
+                            price: "150元",
+                            purchaseDate: "2026年8月13日",
+                            krause: "S2461",
+                            remark: "",
+                            img1: "https://cdn.jsdelivr.net/gh/Tong-Xiangjie/Tong-Xiangjie.github.io@main/notecollection/image/republic_pbkc/A338842-1.jpg",
+                            img2: "https://cdn.jsdelivr.net/gh/Tong-Xiangjie/Tong-Xiangjie.github.io@main/notecollection/image/republic_pbkc/A338842-2.jpg"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
