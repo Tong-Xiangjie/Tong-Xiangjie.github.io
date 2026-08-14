c489ba039d3b8d36b485495f6d43775b365c7c3b
Tong-Xiangjie <2011746785@qq.com>
2026-08-15 00:56:44 +0800

    Create republic_thnb.js

---
+++ notecollection/data/republic_thnb.js
@@ -0,0 +1,50 @@
+// data/republic_thnb.js
+const republic_thnbData = {
+    name: "民国海南银行",
+    icon: null,
+    desc: "The Hainan Bank",
+    detailFields: [
+        { key: "version", label: "冠字号码" },
+        { key: "bank", label: "发行银行" },
+        { key: "print", label: "印刷机构" },
+        { key: "year", label: "发行年份" },
+        { key: "issueDate", label: "发行日期" },
+        { key: "withdrawnDate", label: "停止流通日期" },
+        { key: "size", label: "标准尺寸" },
+        { key: "condition", label: "评级分数" },
+        { key: "price", label: "购入价格" },
+        { key: "purchaseDate", label: "购入日期" },
+        { key: "krause", label: "克劳斯目录编码" }
+    ],
+
+    series: [
+        {
+            seriesName: "中华民国三十八年（1949年） 香港版 银元券",
+            year: "1949",
+            varieties: [
+                {
+                    varietyName: "1949年 5分",
+                    copies: [
+                        {
+                            copyId: 21622387,
+                            year: 1949,
+                            version: "AA",
+                            bank: "海南银行/The Hainan Bank",
+                            print: "香港印字馆印务有限公司/HKPA",
+                            issueDate: "",
+                            withdrawnDate: "",
+                            size: "",
+                            condition: "ACG66E",
+                            price: "48元",
+                            purchaseDate: "2026年8月14日",
+                            krause: "S1453",
+                            remark: "",
+                            img1: "https://cdn.jsdelivr.net/gh/Tong-Xiangjie/Tong-Xiangjie.github.io@main/notecollection/image/republic_thnb/1949-005-1.jpg",
+                            img2: "https://cdn.jsdelivr.net/gh/Tong-Xiangjie/Tong-Xiangjie.github.io@main/notecollection/image/republic_thnb/1949-005-2.jpg"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
