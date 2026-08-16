c127ddecf78bc60f606ff7a6d8c25eee2d779218
Tong-Xiangjie <2011746785@qq.com>
2026-08-16 16:12:30 +0800

    Update special-bridge.js

--- collection/special-bridge.js
+++ collection/special-bridge.js
@@ -1,12 +1,14 @@
-// special-bridge.js
-// 收集各专题的元信息，供 main.js 使用
+// ==================== special-bridge.js ====================
+// 专题配置桥接
+
 window.SPECIAL_CONFIGS = [];
 
 if (typeof specialYearsMeta !== 'undefined') {
     window.SPECIAL_CONFIGS.push(specialYearsMeta);
 }
-
-// 未来新专题在此添加：
-// if (typeof specialXXXMeta !== 'undefined') {
-//     window.SPECIAL_CONFIGS.push(specialXXXMeta);
-// }
+if (typeof specialDenomMeta !== 'undefined') {
+    window.SPECIAL_CONFIGS.push(specialDenomMeta);
+}
+if (typeof specialShanheMeta !== 'undefined') {
+    window.SPECIAL_CONFIGS.push(specialShanheMeta);
+}
