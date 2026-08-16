6dbf443fd3f50a6ebbf758d74a41e997e805dd63
Tong-Xiangjie <2011746785@qq.com>
2026-08-16 16:43:12 +0800

    Update fun-data-bridge.js

--- collection/fun-data-bridge.js
+++ collection/fun-data-bridge.js
@@ -1,8 +1,7 @@
-// ==================== fun-data-bridge.js ====================
-// 趣味收藏数据桥接
-
+// fun-data-bridge.js
+// 收集趣味收藏数据到 window.FUN_DATA_MAP，供专题使用
 window.FUN_DATA_MAP = {
-    yearsData: typeof yearsItems !== 'undefined' ? yearsItems : null,
+    yearsData: typeof banknoteYears !== 'undefined' ? banknoteYears : null,
     denomData: typeof denomItems !== 'undefined' ? denomItems : null,
     shanheData: typeof shanheScenes !== 'undefined' ? shanheScenes : null
 };
