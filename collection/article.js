0e1ebf16df782457fdbea431c992c84610afe9db
Tong-Xiangjie <2011746785@qq.com>
2026-08-17 18:12:08 +0800

    Update article.js

--- collection/article.js
+++ collection/article.js
@@ -317,6 +317,11 @@ function renderArticleSidebar() {
         }
     }
     sidebar.innerHTML = html;
+
+    // ★ 比例压缩（全局函数，定义在 sidebar.js）
+    if (typeof fitSidebarLabels === 'function') {
+        fitSidebarLabels();
+    }
 }
 
 function onArticleSidebarClick(categoryId) {
