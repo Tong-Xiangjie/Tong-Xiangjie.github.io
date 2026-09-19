fef6d4ff3d924d3ffdd36d49004c41cedac511fb
Tong-Xiangjie <2011746785@qq.com>
2026-09-19 18:21:03 +0800

    新增藏品数据

--- notecollection/data/indonesia.js
+++ notecollection/data/indonesia.js
@@ -15,108 +15,150 @@ const indonesiaData = {
         { key: "purchaseDate", label: "购入日期" },
         { key: "krause", label: "纸币目录编号" }
     ],
+    
     series: [
         {
-            seriesName: "2000年版 1000卢比",
-            year: "2016",
-            copies: [
+            seriesName: "1960年系列",
+            year: "1960～1961",
+            varieties: [
                 {
-                    copyId: 25791711,
-                    year: 2016,
-                    version: "FPK115111",
-                    bank: "印度尼西亚银行/Bank Indonesia",
-                    condition: "ACG 67E",
-                    price: "55元",
-                    purchaseDate: "2026年3月10日",
-                    krause: "141n",
-                    size: "141 × 67 mm",
-                    watermark: "Tjut Njak Dien",
-                    remark: "豹子号（数二 金马号 全奇王）",
-                    img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/FPK115111-1.jpg",
-                    img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/FPK115111-2.jpg"
-                }
-            ]
-        },
-        {
-            seriesName: "2016年版 2000卢比",
-            year: "2017",
-            copies: [
-                {
-                    copyId: 26626185,
-                    year: 2017,
-                    version: "FBK599997",
-                    bank: "印度尼西亚银行/Bank Indonesia",
-                    condition: "ACG 67E",
-                    price: "29.5元",
-                    purchaseDate: "2026年2月19日",
-                    krause: "155b",
-                    size: "141 × 67 mm",
-                    watermark: "Mohammad Hoesni Thamrin",
-                    remark: "狮子身（数三 飞马王 全奇王）",
-                    img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/FBK599997-1.jpg",
-                    img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/FBK599997-2.jpg"
-                }
-            ]
-        },
-        {
-            seriesName: "2016年版 100000卢比",
-            year: "2024",
-            copies: [
-                {
-                    copyId: 26626186,
-                    year: 2024,
-                    version: "ORE322444",
-                    bank: "印度尼西亚银行/Bank Indonesia",
-                    condition: "ACG 67E",
-                    price: "92元",
-                    purchaseDate: "2026年2月14日",
-                    krause: "160g",
-                    size: "151 × 66 mm",
-                    watermark: "Soekarno & Mohammad Hatta",
-                    remark: "豹子号（数三 葫芦号 全小号）",
-                    img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/ORE322444-1.jpg",
-                    img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/ORE322444-2.jpg"
+                    varietyName: "2½卢比（Rupiah）",
+                    copies: [
+                        {
+                            copyId: 27104093,
+                            year: 1961,
+                            version: "DVH000196",
+                            bank: "印度尼西亚银行/Bank Indonesia",
+                            condition: "评级中",
+                            price: "20元",
+                            purchaseDate: "2026年3月11日",
+                            krause: "79",
+                            size: "120mm*60mm",
+                            watermark: "无水印/Without watermark",
+                            remark: "",
+                            img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/DVH000196-1.jpg",
+                            img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/DVH000196-2.jpg"
+                        }
+                    ]
                 }
             ]
         },{
-            seriesName: "2022年版 1000卢比",
-            year: "2022",
-            copies: [
+            seriesName: "2000年系列",
+            year: "2000～2016",
+            varieties: [
                 {
-                    copyId: 25255504,
-                    year: 2024,
-                    version: "KAO088999",
-                    bank: "印度尼西亚银行/Bank Indonesia",
-                    condition: "ACG 66E",
-                    price: "76元",
-                    purchaseDate: "2026年5月12日",
-                    krause: "162a",
-                    size: "",
-                    watermark: "",
-                    remark: "这张爱藏标签信息错了^_^，把第三个冠号O当作了数字0，因此标签上面写的是KA0088999，这还使得这张钞票多了一个它本来不该有的版别“连对号”，这也有点搞笑了。不过这一张我是一眼就看上的，所以一开始看到的时候拍卖出价还是3元的时候我就下定决心100以内都可以接受（虽然后来看到出价到了73还是有点肉疼ToT），不过最好还是咬咬牙买到哩~这张的数字冠O是我看上它的最大亮点，况且后面就马上紧邻一个数字0，可以很好地对比这两个字在字体上的差别！这可能才是数字冠一开始本来就应该有的样子吧。",
-                    img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/KAO088999-1.jpg",
-                    img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/KAO088999-2.jpg"
+                    varietyName: "1000卢比（Rupiah）",
+                    copies: [
+                        {
+                            copyId: 25791711,
+                            year: 2016,
+                            version: "FPK115111",
+                            bank: "印度尼西亚银行/Bank Indonesia",
+                            condition: "ACG 67E",
+                            price: "55元",
+                            purchaseDate: "2026年3月10日",
+                            krause: "141n",
+                            size: "",
+                            watermark: "T.N. Dien",
+                            remark: "",
+                            img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/FPK115111-1.jpg",
+                            img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/FPK115111-2.jpg"
+                        }
+                    ]
                 }
             ]
-        },
-        {
-            seriesName: "2020年独立75周年纪念版 75000卢比",
-            year: "2020",
-            copies: [
+        },{
+            seriesName: "2016年系列",
+            year: "2016～2021",
+            varieties: [
                 {
-                    copyId: 15153898,
-                    year: 2020,
-                    version: "AAA829876",
-                    bank: "印度尼西亚银行/Bank Indonesia",
-                    condition: "ACG 67E",
-                    price: "99元",
-                    purchaseDate: "2026年2月7日",
-                    krause: "161",
-                    size: "150 × 65 mm",
-                    watermark: "Soekarno & Mohammad Hatta",
-                    remark: "纪念独立75周年（Commemorative）",
-                    img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/AAA829876-1.jpg",
-                    img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/AAA829876-2.jpg"
+                    varietyName: "2000卢比（Rupiah）",
+                    copies: [
+                        {
+                            copyId: 26626185,
+                            year: 2017,
+                            version: "FBK599997",
+                            bank: "印度尼西亚银行/Bank Indonesia",
+                            condition: "ACG 67E",
+                            price: "29.5元",
+                            purchaseDate: "2026年2月19日",
+                            krause: "155b",
+                            size: "",
+                            watermark: "苏丹提尔塔亚萨/Sultan A. Tirtayasa",
+                            remark: "",
+                            img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/FBK599997-1.jpg",
+                            img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/FBK599997-2.jpg"
+                        }
+                    ]
+                },{
+                    varietyName: "100000卢比（Rupiah）",
+                    copies: [
+                        {
+                            copyId: 26626186,
+                            year: 2024,
+                            version: "ORE322444",
+                            bank: "印度尼西亚银行/Bank Indonesia",
+                            condition: "ACG 67E",
+                            price: "92元",
+                            purchaseDate: "2026年2月14日",
+                            krause: "160g",
+                            size: "",
+                            watermark: "穆罕默德・哈达/M. Hatta",
+                            remark: "",
+                            img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/ORE322444-1.jpg",
+                            img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/ORE322444-2.jpg"
+                        }
+                    ]
+                }
+            ]
+        },{
+            seriesName: "2021年系列",
+            year: "2021～至今",
+            varieties: [
+                {
+                    varietyName: "1000卢比（Rupiah）",
+                    copies: [
+                        {
+                            copyId: 25255504,
+                            year: 2022,
+                            version: "KAO088999",
+                            bank: "印度尼西亚银行/Bank Indonesia",
+                            condition: "ACG 66E",
+                            price: "76元",
+                            purchaseDate: "2026年5月12日",
+                            krause: "162a",
+                            size: "",
+                            watermark: "",
+                            remark: "这张爱藏标签信息错了^_^，把第三个冠号O当作了数字0，因此标签上面写的是KA0088999，这还使得这张钞票多了一个它本来不该有的版别“连对号”，这也有点搞笑了。不过这一张我是一眼就看上的，所以一开始看到的时候拍卖出价还是3元的时候我就下定决心100以内都可以接受（虽然后来看到出价到了73还是有点肉疼ToT），不过最好还是咬咬牙买到哩~这张的数字冠O是我看上它的最大亮点，况且后面就马上紧邻一个数字0，可以很好地对比这两个字在字体上的差别！这可能才是数字冠一开始本来就应该有的样子吧。",
+                            img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/KAO088999-1.jpg",
+                            img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/KAO088999-2.jpg"
+                        }
+                    ]
+                }
+            ]
+        },{
+            seriesName: "纪念钞",
+            year: "",
+            varieties: [
+                {
+                    varietyName: "2020年 印度尼西亚独立75周年纪念钞 75000卢比（Rupiah）",
+                    copies: [
+                        {
+                            copyId: 15153898,
+                            year: 2020,
+                            version: "AAA829876",
+                            bank: "印度尼西亚银行/Bank Indonesia",
+                            condition: "ACG 67E",
+                            price: "99元",
+                            purchaseDate: "2026年2月7日",
+                            krause: "161",
+                            size: "151mm*65mm",
+                            watermark: "苏加诺 - 穆罕默德・哈达/Sukarno - M. Hatta",
+                            remark: "发行日期：2020年8月17日；发行量：7500万。",
+                            img1: "https://tong-xiangjie.github.io/notecollection/image/indonesia/AAA829876-1.jpg",
+                            img2: "https://tong-xiangjie.github.io/notecollection/image/indonesia/AAA829876-2.jpg"
+                        }
+                    ]
                 }
             ]
         }
